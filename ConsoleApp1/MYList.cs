bad792d11a4383a8817db86743c3d6e5667ed76d
Berkay Tuncer <berkay.beren11@gmail.com>
2024-07-08 11:32:02 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/MYList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MYList<T>
+    {
+        T[] items;
+        public MYList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MYList<string> isimler = new MYList<string>();
+            isimler.Add("Berkay");
+        }
+    }
+}
